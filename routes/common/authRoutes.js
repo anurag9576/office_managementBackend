da7f87d39508c984ffa5156f2c689bda7903a483
anuraghamsa <anurag@hamsahitech.com>
2026-03-17 11:03:09 +0530

    feat: Implement employee authentication features including login, registration, and password management, and add static file serving for uploads.

--- routes/common/authRoutes.js
+++ routes/common/authRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { loginEmployee, registerEmployee } = require('../../controllers/common/authController');
+const { loginEmployee, registerEmployee, changePassword } = require('../../controllers/common/authController');
 const { protect, authorize } = require('../../middleware/authMiddleware');
 
 router.post('/login', loginEmployee);
 router.post('/register', protect, authorize('Admin'), registerEmployee);
+router.put('/change-password', protect, changePassword);
 
 module.exports = router;
