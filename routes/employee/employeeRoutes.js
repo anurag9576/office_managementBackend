24e4c526502ddcee5d2afbab19d9af5fca8b95c2
anuraghamsa <anurag@hamsahitech.com>
2026-03-26 14:21:59 +0530

    feat: add employee management API with CRUD operations and next ID generation.

--- routes/employee/employeeRoutes.js
+++ routes/employee/employeeRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { getEmployees, getEmployeeById, updateEmployee, deleteEmployee } = require('../../controllers/employee/employeeController');
+const { getEmployees, getEmployeeById, updateEmployee, deleteEmployee, getNextEmployeeId } = require('../../controllers/employee/employeeController');
 const { protect, authorize, checkPermission } = require('../../middleware/authMiddleware');
 
+router.get('/next-id', protect, getNextEmployeeId);
 router.get('/', protect, checkPermission('employees'), getEmployees);
 router.get('/:id', protect, getEmployeeById);
 router.put('/:id', protect, updateEmployee);
